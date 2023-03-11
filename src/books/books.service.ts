ecc519cc27621ee81f39aebec556c680cfbc7300
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 13:02:09 +0100

    Add Prisma 2nd part

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(private prismaService: PrismaService) {}
+}
