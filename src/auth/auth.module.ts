5bbeda6c5281e37df7bdadb65855c8d32724f563
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 18:00:58 +0100

    Add guard for login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
   imports: [UsersModule],
 })
