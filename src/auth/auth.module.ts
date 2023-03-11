1cb711097a45ede349cc43e8cace223d9a6b37ca
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 18:33:43 +0100

    Add session token(cookie)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,23 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: 'xrwe4543534',
+        signOptions: {
+          expiresIn: '12h',
+        },
+      }),
+    }),
+  ],
 })
 export class AuthModule {}
