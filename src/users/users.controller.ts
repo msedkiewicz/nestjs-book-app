669b9f1ddf6252002a59f5e3d78585a1b18af3e9
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 15:46:00 +0100

    Add users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,21 @@
 import { Controller } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Get, Param, NotFoundException } from '@nestjs/common';
+import { ParseUUIDPipe } from '@nestjs/common/pipes';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Get('/')
+  async getAll() {
+    return this.usersService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const user = await this.usersService.getById(id);
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
 }
