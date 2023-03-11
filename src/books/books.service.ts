425110dfba5aca1dca2557fd0f218666c2c51eac
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 17:23:56 +0100

    Move Prisma to Prisma Module; update imports

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { Book } from '@prisma/client';
-import { PrismaService } from 'src/shared/services/prisma.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class BooksService {
