358598b08fe7625bc159e0139dbd6f75c11c434f
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 17:25:31 +0100

    Add data passing logic for Auth / Users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   imports: [PrismaModule],
+  exports: [UsersService],
 })
 export class UsersModule {}
