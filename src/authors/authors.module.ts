d6001a4a16a0918c86a69fe0d9a4d5e4e6f666f2
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 12:59:56 +0100

    Add Prisma to authors and books modules

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthorsController } from './authors.controller';
 import { AuthorsService } from './authors.service';
+import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Module({
   controllers: [AuthorsController],
-  providers: [AuthorsService]
+  providers: [AuthorsService, PrismaService],
 })
 export class AuthorsModule {}
