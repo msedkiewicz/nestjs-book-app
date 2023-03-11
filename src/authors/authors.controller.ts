feb40368c30135675a3092ca9eb20891c3257793
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 14:56:11 +0100

    Add PUT endpoint for author

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -5,10 +5,12 @@ import {
   NotFoundException,
   Post,
   Body,
+  Put,
 } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { ParseUUIDPipe } from '@nestjs/common/pipes';
 import { CreateAuthorDTO } from './dtos/create-author.dto';
+import { UpdateAuthorDTO } from './dtos/update-author.dto';
 
 @Controller('authors')
 export class AuthorsController {
@@ -29,5 +31,17 @@ export class AuthorsController {
   @Post('/')
   create(@Body() authorData: CreateAuthorDTO) {
     return this.authorsService.create(authorData);
+  }
+
+  @Put('/:id')
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() authorData: UpdateAuthorDTO,
+  ) {
+    if (!(await this.authorsService.getById(id)))
+      throw new NotFoundException('Author not found');
+
+    await this.authorsService.updateById(id, authorData);
+    return { success: true };
   }
 }
